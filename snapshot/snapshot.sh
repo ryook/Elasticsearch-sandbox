389b57ca4b80999359ac679e1c3ba7c2e2686417
ryokato <ryo.kato23@gmail.com>
2022-12-22 23:44:38 +0900

    update command

--- snapshot/snapshot.sh
+++ snapshot/snapshot.sh
@@ -4,13 +4,7 @@
 url="http://localhost:9200"
 index_name="test_index"
 
-curl -XPUT -H 'Content-Type: application/json' ${url}/_snapshot/fs_backup/snapshot -d @- <<EOS
-{
-    "indices": "${index_name}"
-}
-EOS
-
-curl -XPUT -H 'Content-Type: application/json' ${url}/_snapshot/fs_backup/snapshot2 -d @- <<EOS
+curl -XPUT -H 'Content-Type: application/json' ${url}/_snapshot/fs_backup/my_snapshot_1 -d @- <<EOS
 {
     "indices": "${index_name}",
     "include_global_state": false
